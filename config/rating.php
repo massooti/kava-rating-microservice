9ca71cf7fb8e09d687e1d393a06d243af7ed9a99
zaeem@pc.co <zaeem@pc.co>
2021-04-15 11:28:38 +0430

    fix confilcts

--- config/rating.php
+++ config/rating.php
@@ -18,4 +18,27 @@ return [
     'password' => env('RATING_MICROSERVICE_PASSWORD', ''),
 
 
+
+    /*
+     * Settings for `laravel` type output.
+     */
+    'routes' => [
+        /*
+         * Whether to automatically create a docs endpoint for you to view your generated docs.
+         * If this is false, you can still set up routing manually.
+         */
+        'add_routes' => true,
+
+        /*
+         * URL path to use for the docs endpoint (if `add_routes` is true).
+         * By default, `/docs` opens the HTML page, `/docs.postman` opens the Postman collection, and `/docs.openapi` the OpenAPI spec.
+         */
+        'prefix_url' => '/api/v1/',
+
+        /*
+         * Middleware to attach to the docs endpoint (if `add_routes` is true).
+         */
+        'middleware' => '',
+    ],
+
 ];
--- routes/api.php
+++ routes/api.php
@@ -1,15 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Kavano\Rating\RatingController;
+$prefix = config('rating.routes.prefix_url');
+$middleware = config('rating.routes.middleware');
 
-Route::namespace('\Kavano\Rating\Http')
-
-    ->group(function () use ($prefix) {
-        Route::group(['prefix' => 'rating'], function () {
-            Route::get('/status', 'RateController@getRate'); // get user rate status
-            Route::post('/submit', 'RateController@submitRate'); // user submit rates and feedback to the app
-        });
-    
+Route::group(['prefix' => $prefix, 'middleware' => $middleware], function () {
+    Route::group(['prefix' => 'rating'], function () {
+        Route::get('/status', [RatingController::class, 'getRate'])->name('get-rate-status'); // get user rate status,
+        Route::post('/submit', [RatingController::class, 'submitRate'])->name('submit-rate'); // user submit rates and feedback to the app
     });
-
-    
+});
